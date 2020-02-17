47c83807bba9e54221a848b5b8e9d5a1e4b79843
Mahbub Mozadded <m.mozadded@cimsolutions.nl>
2020-02-17 11:16:35 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React here 2
+          Learn React 2
         </a>
       </header>
     </div>
